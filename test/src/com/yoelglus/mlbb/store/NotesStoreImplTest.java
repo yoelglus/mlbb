76f503131f68d23d74ce9fe8828d5163a8563042
Yoel Gluschnaider <yoelglus@gmail.com>
2014-12-07 14:11:03 +0000

    Some more tests added.

--- test/src/com/yoelglus/mlbb/store/NotesStoreImplTest.java
+++ test/src/com/yoelglus/mlbb/store/NotesStoreImplTest.java
@@ -4,7 +4,7 @@ import com.yoelglus.mlbb.models.Note;
 import org.junit.Before;
 import org.junit.After;
 import org.junit.Test;
-import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.assertThat;
 
 public class NotesStoreImplTest {
@@ -36,7 +36,17 @@ public class NotesStoreImplTest {
     }
 
     @Test
-    public void testLoadNote() throws Exception {
-
+    public void testSaveNotesWithText()
+    {
+        Note note1 = new Note("This is note 1");
+        Note note2 = new Note("This is note 2");
+        Note note3 = new Note("This is note 1");
+        int note1Id = mNoteStore.saveNote(note1);
+        int note2Id = mNoteStore.saveNote(note2);
+        int note3Id = mNoteStore.saveNote(note3);
+        assertThat(mNoteStore.loadNote(note1Id), equalTo(note1));
+        assertThat(mNoteStore.loadNote(note2Id), equalTo(note2));
+        assertThat(mNoteStore.loadNote(note3Id), equalTo(note3));
+        assertThat(mNoteStore.loadNote(note1Id), not(equalTo(note3)));
     }
 }
