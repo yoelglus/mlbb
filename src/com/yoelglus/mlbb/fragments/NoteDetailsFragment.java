dcc9e8f443b200c4abf7b3334ff1e562305ac88f
Yoel Gluschnaider <yoelglus@gmail.com>
2015-01-04 17:47:50 +0000

    Implement NotesDetailsFragment.

--- src/com/yoelglus/mlbb/fragments/NoteDetailsFragment.java
+++ src/com/yoelglus/mlbb/fragments/NoteDetailsFragment.java
@@ -5,9 +5,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import com.yoelglus.mlbb.R;
+import com.yoelglus.mlbb.dummy.DummyContent;
 
 public class NoteDetailsFragment extends Fragment {
     public static final String ARG_NOTE_ID = "note_id";
+    private int noteId;
 
     public static NoteDetailsFragment create(int noteId) {
         Bundle arguments = new Bundle();
@@ -18,7 +22,22 @@ public class NoteDetailsFragment extends Fragment {
     }
 
     @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() == null || !getArguments().containsKey(NoteDetailsFragment.ARG_NOTE_ID))
+            throw new IllegalArgumentException("Arguments should contain note ID!");
+        noteId = getArguments().getInt(NoteDetailsFragment.ARG_NOTE_ID, -1);
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return new View(getActivity());
+        View rootView = inflater.inflate(R.layout.fragment_note_details, container, false);
+        TextView contentView = (TextView) rootView.findViewById(R.id.note_content);
+        contentView.setText(DummyContent.notesMap.get(noteId).content);
+        return rootView;
+    }
+
+    public int getNoteId() {
+        return noteId;
     }
 }
