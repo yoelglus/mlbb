8a2fc86b23988cb311f32271983e46d82c89eb52
Yoel Gluschnaider <yoelglus@gmail.com>
2015-01-04 16:05:14 +0000

    Implemented notes list fragment.

--- test/src/com/yoelglus/mlbb/fragments/NotesListFragmentTest.java
+++ test/src/com/yoelglus/mlbb/fragments/NotesListFragmentTest.java
@@ -1,19 +1,21 @@
 package com.yoelglus.mlbb.fragments;
 
+import android.app.Activity;
+import android.app.FragmentManager;
 import android.widget.ListView;
 import android.widget.TextView;
 import com.yoelglus.mlbb.dummy.DummyContent;
-import com.yoelglus.mlbb.fragments.NotesListFragment;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
+import org.robolectric.util.ActivityController;
 
 import java.util.LinkedList;
 import java.util.List;
 
-import static org.junit.Assert.*;
-import static org.robolectric.util.FragmentTestUtil.startFragment;
+import static org.junit.Assert.assertEquals;
 
 @RunWith(RobolectricTestRunner.class)
 @Config(manifest = "../AndroidManifest.xml")
@@ -21,6 +23,7 @@ public class NotesListFragmentTest {
 
     private static final int TEST_ITEMS_COUNT = 6;
     private static final int TEST_NOTE_INDEX = 1;
+    private NotesListFragmentContainingActivity containingActivity;
 
     @Test
     public void testEmptyListCreatesEmptyAdapter() throws Exception {
@@ -38,12 +41,25 @@ public class NotesListFragmentTest {
 
     @Test
     public void testListItemViewsContent() throws Exception {
+        ListView notesListView = createFragmentAndGetListView();
+        TextView noteTextView = (TextView) notesListView.getChildAt(TEST_NOTE_INDEX);
+        assertEquals("Note " + TEST_NOTE_INDEX, noteTextView.getText().toString());
+    }
+
+    @Test
+    public void testSelectingItemInvokesCallback() throws Exception {
+        ListView notesListView = createFragmentAndGetListView();
+        TextView noteTextView = (TextView) notesListView.getChildAt(TEST_NOTE_INDEX);
+        notesListView.performItemClick(noteTextView, TEST_NOTE_INDEX, 0);
+        assertEquals(TEST_NOTE_INDEX, containingActivity.selectedNoteId);
+    }
+
+    private ListView createFragmentAndGetListView() {
         createDummyNotesList(TEST_ITEMS_COUNT);
         NotesListFragment notesListFragment = createAndStartNotesListFragment();
         ListView notesListView = notesListFragment.getListView();
         forceListItemsCreation(notesListView);
-        TextView noteTextView = (TextView) notesListView.getChildAt(TEST_NOTE_INDEX);
-        assertEquals("Note " + TEST_NOTE_INDEX, noteTextView.getText().toString());
+        return notesListView;
     }
 
     private void forceListItemsCreation(ListView notesListView) {
@@ -52,7 +68,9 @@ public class NotesListFragmentTest {
 
     private NotesListFragment createAndStartNotesListFragment() {
         NotesListFragment notesListFragment = new NotesListFragment();
-        startFragment(notesListFragment);
+        containingActivity = createContainingActivity();
+        FragmentManager fragmentManager = containingActivity.getFragmentManager();
+        fragmentManager.beginTransaction().add(notesListFragment, null).commit();
         return notesListFragment;
     }
 
@@ -61,5 +79,22 @@ public class NotesListFragmentTest {
         for (int i=0; i<itemsCount; i++)
             notes.add(new DummyContent.DummyNote(i, "Note " + i));
         DummyContent.notes = notes;
+    }
+
+    private static NotesListFragmentContainingActivity createContainingActivity() {
+        ActivityController<NotesListFragmentContainingActivity> controller =
+                Robolectric.buildActivity(NotesListFragmentContainingActivity.class);
+        controller.create().start().resume();
+        return controller.get();
+    }
+
+    private static class NotesListFragmentContainingActivity extends Activity implements NotesListFragment.Callbacks {
+
+        private int selectedNoteId;
+
+        @Override
+        public void onNoteSelected(int noteId) {
+            selectedNoteId = noteId;
+        }
     }
 }
