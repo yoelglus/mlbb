2e200275efa342a6cc567f420519f8834065c388
Yoel Gluschnaider <yoelglus@gmail.com>
2016-09-03 17:13:03 +0100

    Now observes on ioScheduler

--- domain/src/main/kotlin/com/yoelglus/mlbb/domain/GetNotes.kt
+++ domain/src/main/kotlin/com/yoelglus/mlbb/domain/GetNotes.kt
@@ -2,11 +2,12 @@ package com.yoelglus.mlbb.domain
 
 import com.yoelglus.mlbb.domain.datastore.NotesStore
 import com.yoelglus.mlbb.entities.Note
+import rx.Scheduler
 import rx.Subscriber
 
-class GetNotes(val notesStore: NotesStore) {
+class GetNotes(val notesStore: NotesStore, val ioScheduler: Scheduler) {
     fun execute(subscriber: Subscriber<List<Note>>) {
-        notesStore.getNotes().subscribe(subscriber)
+        notesStore.getNotes().observeOn(ioScheduler).subscribe(subscriber)
     }
 
 }
--- domain/src/test/kotlin/com/yoelglus/mlbb/domain/Get Notes Test.kt
+++ domain/src/test/kotlin/com/yoelglus/mlbb/domain/Get Notes Test.kt
@@ -1,42 +1,64 @@
 package com.yoelglus.mlbb.domain
 
-import com.nhaarman.mockito_kotlin.doReturn
 import com.nhaarman.mockito_kotlin.mock
+import com.nhaarman.mockito_kotlin.whenever
 import com.yoelglus.mlbb.domain.datastore.NotesStore
 import com.yoelglus.mlbb.entities.Note
 import org.hamcrest.CoreMatchers.`is`
 import org.junit.Assert.assertThat
+import org.junit.Before
 import org.junit.Test
 import rx.Observable
 import rx.observers.TestSubscriber
+import rx.schedulers.TestScheduler
 
 class `Get Notes Test` {
 
+    private lateinit var getNotes: GetNotes
+    private lateinit var notesStore: NotesStore
+    private val testSubscriber = TestSubscriber<List<Note>>()
+    private val ioScheduler = TestScheduler()
+
+    @Before
+    fun setUp() {
+        notesStore = mock()
+        getNotes = GetNotes(notesStore, ioScheduler)
+    }
+
     @Test
     fun `should return empty list of notes`() {
-        val notesStore = mock<NotesStore>() {
-            on { getNotes() } doReturn Observable.just(emptyList())
-        }
-        val subscriber = TestSubscriber<List<Note>>()
+        givenNotesStoreWillReturn(emptyList())
 
-        val getNotes = GetNotes(notesStore)
-        getNotes.execute(subscriber)
+        getNotes.execute(testSubscriber)
+        ioScheduler.triggerActions()
 
-        assertThat(subscriber.onNextEvents[0], `is`(emptyList()))
+        testSubscriber.assertReceivedOnNext(listOf(emptyList()))
     }
 
     @Test
     fun `should return the list`() {
-        val notesStore = mock<NotesStore>() {
-            on { getNotes() } doReturn Observable.just(listOf(Note("first"), Note("second")))
-        }
-        val subscriber = TestSubscriber<List<Note>>()
+        givenNotesStoreWillReturn(listOf(Note("first"), Note("second")))
 
-        val getNotes = GetNotes(notesStore)
-        getNotes.execute(subscriber)
+        getNotes.execute(testSubscriber)
+        ioScheduler.triggerActions()
 
-        assertThat(subscriber.onNextEvents[0], `is`(listOf(Note("first"), Note("second"))))
+        testSubscriber.assertReceivedOnNext(listOf(listOf(Note("first"), Note("second"))))
+    }
 
+    @Test
+    fun `should observe on io scheduler`() {
+        givenNotesStoreWillReturn(listOf(Note("first"), Note("second")))
+
+        getNotes.execute(testSubscriber)
+        testSubscriber.assertNoValues()
+        ioScheduler.triggerActions()
+
+        testSubscriber.assertReceivedOnNext(listOf(listOf(Note("first"), Note("second"))))
+
+    }
+
+    private fun givenNotesStoreWillReturn(notes: List<Note>) {
+        whenever(notesStore.getNotes()).thenReturn(Observable.just(notes))
     }
 }
 
